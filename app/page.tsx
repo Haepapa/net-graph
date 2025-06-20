a2d483b307ef7a2d914b3a0ca89226d2b4b230f8
timotewb <timote.wb@gmail.com>
2025-06-20 13:11:03 +1200

    dev: update for regraph - working

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import type { GraphCanvasRef, GraphNode, GraphEdge } from "reagraph";
 import { useSelection } from "reagraph";
 const GraphCanvas = dynamic(
@@ -10,53 +10,21 @@ const GraphCanvas = dynamic(
 
 export default function Home() {
   const graphRef = useRef<GraphCanvasRef | null>(null);
-  const nodes: GraphNode[] = [
-    {
-      id: "n-1",
-      label: "...meri lis file upload.egp",
-      fill: "#e66557",
-      type: "SAS Enterprise Guide",
-      name: "data/get/Data meri lis file upload.egp",
-      size: 10,
-    },
-    {
-      id: "n-2",
-      label: "2",
-      size: 3,
-    },
-    {
-      id: "n-3",
-      label: "3",
-      size: 20,
-    },
-    {
-      id: "n-4",
-      label: "4",
+  const [nodes, setNodes] = useState<GraphNode[]>([]);
+  const [edges, setEdges] = useState<GraphEdge[]>([]);
 
-      size: 12,
-    },
-  ];
-
-  const edges: GraphEdge[] = [
-    {
-      id: "1->2",
-      source: "n-1",
-      target: "n-2",
-      label: "Edge 1-2",
-    },
-    {
-      id: "2->3",
-      source: "n-2",
-      target: "n-3",
-      label: "Edge 2-3",
-    },
-    {
-      id: "2->4",
-      source: "n-2",
-      target: "n-4",
-      label: "Edge 2-4",
-    },
-  ];
+  useEffect(() => {
+    fetch("/data/nodes.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setNodes(data);
+      });
+    fetch("/data/edges.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setEdges(data);
+      });
+  }, []);
 
   const { selections, actives, onNodeClick, onCanvasClick } = useSelection({
     ref: graphRef,
